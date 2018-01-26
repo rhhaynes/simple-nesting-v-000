990ff532fc3b29d1c68f1cd906e3874cbc01129c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-26 15:11:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,13 +56,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-	programmer_hash = 
+  programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -77,8 +71,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash[:yukihiro_matsumoto] => {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
